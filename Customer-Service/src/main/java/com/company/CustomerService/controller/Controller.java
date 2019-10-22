ed5b4a1b947d113b6b9aaedf85ee88c1326dfbad
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-22 14:35:33 -0400

    Adding  application properties for Test

--- Customer-Service/src/main/java/com/company/CustomerService/controller/Controller.java
+++ Customer-Service/src/main/java/com/company/CustomerService/controller/Controller.java
@@ -1,10 +1,50 @@
 package com.company.CustomerService.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.company.CustomerService.dao.CustomerDao;
+import com.company.CustomerService.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
+@RefreshScope
+@RequestMapping("/customers")
+@CacheConfig(cacheNames = {"customers"})
 public class Controller {
+    @Autowired
+    CustomerDao customerDao;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer createCustomer(@RequestBody @Valid Customer customer){
+        return null;
+    }
 
+    @GetMapping("/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId){
+        return null;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers(){
+        return null;
+    }
+
+    @PutMapping
+    public void updateCustomer(@RequestBody Customer customer){
+    }
+
+    @DeleteMapping("/{customerId}")
+    public void deleteCustomer(@PathVariable int customerId){
+
+    }
 }
--- Customer-Service/src/main/java/com/company/CustomerService/dao/CustomerDao.java
+++ Customer-Service/src/main/java/com/company/CustomerService/dao/CustomerDao.java
@@ -6,13 +6,4 @@ import java.util.List;
 
 public class CustomerDao {
 
-    Customer addCustomer(Customer customer);
-
-    Customer getCustomer(int customerId);
-
-    void deleteCustomer(int customerId);
-
-    void updateCustomer(Customer customer);
-
-    List<Customer> getAllCustomer();
 }
