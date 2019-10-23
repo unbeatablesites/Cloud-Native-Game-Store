da0ab39778c465d2ff8638476994e2148471c0c9
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-23 15:24:38 -0400

    Adding model for inventory

---
+++ inventory-service/src/main/java/com/company/inventoryservice/dto/Inventory.java
@@ -0,0 +1,48 @@
+package com.company.inventoryservice.dto;
+
+import java.util.Objects;
+
+public class Inventory {
+    private int inventoryId;
+    private Integer productId;
+    private Integer quantity;
+
+    public int getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Inventory inventory = (Inventory) o;
+        return inventoryId == inventory.inventoryId &&
+                Objects.equals(productId, inventory.productId) &&
+                Objects.equals(quantity, inventory.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(inventoryId, productId, quantity);
+    }
+}
