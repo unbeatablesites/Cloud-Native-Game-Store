e1ebc7d83e934492e6695206b51a1abbecde6929
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-23 14:00:36 -0400

    Added test

--- invoice-service/src/main/java/com/company/invoiceservice/dao/InvoiceDaoJdbcTemplateImpl.java
+++ invoice-service/src/main/java/com/company/invoiceservice/dao/InvoiceDaoJdbcTemplateImpl.java
@@ -4,12 +4,13 @@ import com.company.invoiceservice.dto.Invoice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
+@Repository
 public class InvoiceDaoJdbcTemplateImpl implements InvoiceDao{
 
     private JdbcTemplate jdbcTemplate;
