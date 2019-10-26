afe14fbee21df7f16e17d4c3cf4e16ef02bc08bf
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-26 13:30:39 -0400

    Adding dto for retail admin api

---
+++ admin-api/src/main/java/com/company/adminapi/dto/Customer.java
@@ -0,0 +1,129 @@
+package com.company.adminapi.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Customer implements Serializable {
+
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String zip;
+    private String email;
+    private String phone;
+
+    public Customer(){
+    }
+
+    public Customer(String firstName,String lastName, String street, String city, String zip, String email, String phone){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.street=street;
+        this.city=city;
+        this.zip=zip;
+        this.email=email;
+        this.phone=phone;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return customerId == customer.customerId &&
+                Objects.equals(firstName, customer.firstName) &&
+                Objects.equals(lastName, customer.lastName) &&
+                Objects.equals(street, customer.street) &&
+                Objects.equals(city, customer.city) &&
+                Objects.equals(zip, customer.zip) &&
+                Objects.equals(email, customer.email) &&
+                Objects.equals(phone, customer.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId, firstName, lastName, street, city, zip, email, phone);
+    }
+
+    @Override
+    public String toString(){
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName=" + firstName +
+                ", lastName='" + lastName +
+                ", street='" + street +
+                ", city='" + city +
+                ", zip='" + zip +
+                ", email='" + email +
+                ", phone='" + phone +
+                '\'' +
+                '}';
+    }
+}
