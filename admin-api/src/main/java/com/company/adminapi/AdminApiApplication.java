b0c70fbe4c9a0391cee5df9129a331b6477287d7
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-26 13:53:22 -0400

    Adding view model for retail admin api

--- admin-api/src/main/java/com/company/adminapi/AdminApiApplication.java
+++ admin-api/src/main/java/com/company/adminapi/AdminApiApplication.java
@@ -1,9 +1,15 @@
 package com.company.adminapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class AdminApiApplication {
 
 	public static void main(String[] args) {
